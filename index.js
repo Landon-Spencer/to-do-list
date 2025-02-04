f8ab68fc634dae2fac4f4964e4d5789bdaf8bb5d
Landon-Spencer <landon.r.spencer@gmail.com>
2025-02-04 11:02:23 -0700

    added input for To-Do items and added to list

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const toDoItems = document.getElementById('toDoItems');
+  const addItem = document.getElementById('addItem');
+  const addButton = document.getElementById('addItemButton');
+
+  addButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const item = addItem.value;
+    const newItemElement = document.createElement('input');
+    newItemElement.type = 'checkbox';
+    newItemElement.id = item;
+    const newItemLabel = document.createElement('label');
+    newItemLabel.htmlFor = item;
+    newItemLabel.innerHTML = item;
+    toDoItems.appendChild(newItemElement);
+    toDoItems.appendChild(newItemLabel);
+    toDoItems.appendChild(document.createElement('br'));
+    addItem.value = '';
+  });
+
+});
+
