9f66f81d1c061b7b40ba02165e99198059574a32
Landon-Spencer <landon.r.spencer@gmail.com>
2025-02-04 13:24:26 -0700

    added completed list

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const toDoItems = document.getElementById('toDoItems');
+  const completedItems = document.getElementById('listOfCompletedItems');
   const addItem = document.getElementById('addItem');
   const addButton = document.getElementById('addItemButton');
 
@@ -17,15 +18,21 @@ document.addEventListener('DOMContentLoaded', () => {
     newItemButton.innerHTML = 'Remove Item';
     newItemButton.addEventListener('click', (event) => {
       event.preventDefault();
+      const newCompletedItem = document.createElement('li');
+      newCompletedItem.id = `${item}`;
+      newCompletedItem.innerHTML = item;
+      completedItems.appendChild(newCompletedItem);
       toDoItems.removeChild(newItemElement);
       toDoItems.removeChild(newItemLabel);
       toDoItems.removeChild(newItemButton);
-      toDoItems.removeChild(document.querySelector('br'));
+      toDoItems.removeChild(document.getElementById(`${item} toDo br`));
     })
+    const newBreak = document.createElement('br');
+    newBreak.id = `${item} toDo br`;
     toDoItems.appendChild(newItemElement);
     toDoItems.appendChild(newItemLabel);
     toDoItems.appendChild(newItemButton);
-    toDoItems.appendChild(document.createElement('br'));
+    toDoItems.appendChild(newBreak);
     addItem.value = '';
   });
 });
